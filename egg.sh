10f38415447f89b0e0427138e0f06c5969079977
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2026-09-11 22:00:42 -0400

    Add files via upload
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

---
+++ egg.sh
@@ -0,0 +1,13 @@
+./auto_parser_ternary 2 "awa" :samu '# ################################################################################### #
+#                                       Episcript                                     #
+# ################################################################################### #
+samu sabo Eat Burn Toss apple banana lemon you I she apple banana lemon you I she eat burn toss
+samu sabo Eat Burn Toss apple banana lemon you I she apple banana lemon you I she eat burn toss' :samu '# ################################################################################### #
+#                                       Episcript                                     #
+# ################################################################################### #
+sama sabo Eat Burn Toss apple banana lemon you I she apple banana lemon you I she eat burn toss
+sama sabo Eat Burn Toss apple banana lemon you I she apple banana lemon you I she eat burn toss' :hikasu '# ################################################################################### #
+#                                       Episcript                                     #
+# ################################################################################### #
+hikasu sabo Eat Burn Toss apple banana lemon you I she apple banana lemon you I she eat burn toss
+hikasu sabo Eat Burn Toss apple banana lemon you I she apple banana lemon you I she eat burn toss'
